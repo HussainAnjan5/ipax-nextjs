902539cbd545b538ce2145505f561da01dcf037c
HussainAnjan5 <muhammad.hussain.cs8@gmail.com>
2024-09-27 08:18:14 -0700

    home commit

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+const AddtoCart = () => {
+  return (
+    <div>
+      <button className='bg-black text-white rounded-lg p-3' onClick={() => console.log('Add to Cart')}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default AddtoCart;
